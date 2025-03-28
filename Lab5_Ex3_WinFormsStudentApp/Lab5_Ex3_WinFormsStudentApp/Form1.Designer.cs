52a59af2f996e1f847f8d286103917736f87fc49
Nan Hnin Yai Kyi <itsjustnnyk968@gmail.com>
2025-03-28 20:33:08 +0700

    Added Label and TextBox for Student Name.

--- Lab5_Ex3_WinFormsStudentApp/Lab5_Ex3_WinFormsStudentApp/Form1.Designer.cs
+++ Lab5_Ex3_WinFormsStudentApp/Lab5_Ex3_WinFormsStudentApp/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtStudentName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.AutoSize = true;
+            this.txtStudentName.Location = new System.Drawing.Point(143, 167);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(92, 16);
+            this.txtStudentName.TabIndex = 0;
+            this.txtStudentName.Text = "Student Name";
+            this.txtStudentName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(271, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtStudentName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label txtStudentName;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Lab5_Ex3_WinFormsStudentApp/Lab5_Ex3_WinFormsStudentApp/Form1.cs
+++ Lab5_Ex3_WinFormsStudentApp/Lab5_Ex3_WinFormsStudentApp/Form1.cs
@@ -16,5 +16,10 @@ namespace Lab5_Ex3_WinFormsStudentApp
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
